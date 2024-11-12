a31552e6d9593bb66e4a0fe946b03b844e7efdb5
YOONSOO7429 <125416958+YOONSOO7429@users.noreply.github.com>
2024-11-12 15:38:56 +0900

    fix: 강좌 등록 완료 디자인 변경

--- src/app/(afterLogin)/schedule/(general)/add/complete/[id]/_components/main/index.tsx
+++ src/app/(afterLogin)/schedule/(general)/add/complete/[id]/_components/main/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { CourseDetailProps, ProfileWithTypeProps } from "@types";
 
 import styled from "./styles.module.scss";
+import { DAY_ENG_TO_KOR } from "@data";
 
 export function Main({
   courseData,
@@ -13,6 +14,12 @@ export function Main({
   courseData: CourseDetailProps;
   instructorData: ProfileWithTypeProps;
 }) {
+  // 요일 변환
+  const translatedDays = courseData.courseDays
+    .split(",")
+    .map((day) => DAY_ENG_TO_KOR[day.trim() as keyof typeof DAY_ENG_TO_KOR])
+    .join(",");
+
   return (
     <div className={styled.content}>
       <div className={styled.title_wrapper}>
@@ -31,6 +38,24 @@ export function Main({
         <p>
           <span>{instructorData.instructor[0].instructorName}</span> 강사님 수업
         </p>
+      </div>
+      <div className={styled.courseInfo1}>
+        <p className={styled.days}>{translatedDays}</p>
+        <p className={styled.time}>{courseData.courseStartTime}</p>
+        <span>-</span>
+        <p className={styled.time}>{courseData.courseEndTime}</p>
+      </div>
+      <div className={styled.courseInfo2}>
+        <p className={styled.capacity}>
+          참여가능 인원: {courseData.courseCapacity}
+        </p>
+        <div className={styled.color}>
+          <div
+            className={styled.color_sample}
+            style={{ backgroundColor: courseData.courseColor }}
+          />
+          <p>{courseData.courseColor}</p>
+        </div>
       </div>
     </div>
   );
